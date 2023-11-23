492faf0b850733fa036ddfa64e27a2c78f1c6e17
amartinezm90 <a.martinez.m90@gmail.com>
2023-11-23 14:39:59 +0100

    added pandas and numpy

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        # Aquí puedes listar las dependencias de tu librería, por ejemplo:
-        # 'numpy',
-        # 'pandas',
+        'numpy',
+        'pandas'
     ],
     # Más configuraciones como author, license, etc.
 )
