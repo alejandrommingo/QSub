6ce25aa7e94f91388e063be9ba0bfef0154e0db3
amartinezm90 <a.martinez.m90@gmail.com>
2023-11-29 14:08:43 +0100

    deserved_neighbors function created. Resources uploaded

--- QSub/contours.py
+++ QSub/contours.py
@@ -1,12 +1,41 @@
 import requests
 import re
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from fuzzywuzzy import process
 import xml.etree.ElementTree as ET
 import html
 from numpy.linalg import norm
 
 
-def gallito_neighbors_matrix(word, gallito_code, space_name, neighbors=100, min_cosine_contour=0.3, space_dimensions = 300):
+def gallito_neighbors_matrix(word, gallito_code, space_name, neighbors=100, min_cosine_contour=0.3, space_dimensions=300):
+    """
+    Consulta un espacio semántico específico (denominado 'Gallito') para obtener
+    una matriz de los vecinos semánticos más cercanos a un término dado. Extrae los
+    vectores semánticos de los términos vecinos y filtra estos vecinos basándose
+    en una similitud coseno mínima especificada.
+
+    :param word: El término objetivo para el cual se buscan vecinos semánticos.
+    :type word: str
+    :param gallito_code: Código de identificación para acceder al espacio semántico de Gallito.
+    :type gallito_code: str
+    :param space_name: Nombre del espacio semántico dentro del servicio de Gallito.
+    :type space_name: str
+    :param neighbors: Número de vecinos semánticos a recuperar. Por defecto es 100.
+    :type neighbors: int
+    :param min_cosine_contour: El umbral mínimo de similitud coseno para considerar un vecino relevante. Por defecto es 0.3.
+    :type min_cosine_contour: float
+    :param space_dimensions: Número de dimensiones del espacio semántico. Por defecto es 300.
+    :type space_dimensions: int
+    :return: Un diccionario que contiene dos claves: 'neighbors', una lista de términos vecinos que cumplen con el criterio de similitud coseno mínimo, y 'neighbors_vec', una matriz numpy donde cada fila representa el vector semántico de los términos vecinos seleccionados.
+    :rtype: dict
+
+    Notas Adicionales:
+    Esta función depende de los módulos 'requests', 'numpy', 're' y 'html' para realizar solicitudes HTTP,
+    procesamiento de matrices y cadenas. Asegúrate de que el código de Gallito y el nombre del espacio
+    semántico sean correctos y estén actualizados para el correcto funcionamiento de la función.
+    """
     k = space_dimensions  # Define K dimensions
     n = neighbors  # Define N neighbors
 
@@ -81,6 +110,23 @@ def gallito_neighbors_matrix(word, gallito_code, space_name, neighbors=100, min_
     return results
 
 def word_vector(word, gallito_code, space_name):
+    """
+    Extrae el vector semántico de un término específico desde un espacio semántico
+    proporcionado, utilizando el servicio web de Gallito.
+
+    :param word: El término objetivo para el cual se busca el vector semántico.
+    :type word: str
+    :param gallito_code: Código de identificación para acceder al espacio semántico de Gallito.
+    :type gallito_code: str
+    :param space_name: Nombre del espacio semántico dentro del servicio de Gallito.
+    :type space_name: str
+    :return: Un array numpy que representa el vector semántico del término dado.
+    :rtype: numpy.ndarray
+
+    La función realiza una solicitud GET al servicio web de Gallito para obtener el
+    vector del término especificado. Los valores del vector se extraen de la respuesta
+    XML, se decodifican las entidades HTML y se convierten los valores numéricos a float.
+    """
     # Extraer el vector del término objetivo
     resp_a = requests.get(
         f"http://psicoee.uned.es/{space_name}/Service.svc/webHttp/getVectorOfTerm?code={gallito_code}&a={word}")
@@ -96,7 +142,21 @@ def word_vector(word, gallito_code, space_name):
 
 
 def word_cosine_similarity(v1, v2):
-    """Calcula la similitud coseno entre dos vectores."""
+    """
+    Calcula la similitud coseno entre dos vectores.
+
+    :param v1: Primer vector para el cálculo de la similitud coseno.
+    :type v1: numpy.ndarray
+    :param v2: Segundo vector para el cálculo de la similitud coseno.
+    :type v2: numpy.ndarray
+    :return: El valor de la similitud coseno entre los dos vectores.
+    :rtype: float
+
+    Esta función calcula la similitud coseno, una medida de similitud entre dos vectores
+    no nulos en un espacio que tiene en cuenta la orientación de los vectores pero no su magnitud.
+    La similitud coseno se define como el producto punto de los vectores dividido por el producto
+    de sus magnitudes (normas).
+    """
     cos_sim = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
     return cos_sim
 
@@ -107,9 +167,20 @@ def neighbors_similarity(word_semantic_vector, word_neighbors_matrix):
     en una matriz de vecinos, ordenando los resultados en orden descendente. También devuelve
     los términos asociados en el mismo orden.
 
-    :param word_semantic_vector: ndarray, el vector semántico de la palabra objetivo.
-    :param word_neighbors_matrix: dict, con "neighbors_vec" (ndarray) y "neighbors" (lista de términos).
-    :return: Tuple de dos ndarrays, el primero de similitudes coseno y el segundo de términos, ambos ordenados.
+    :param word_semantic_vector: El vector semántico de la palabra objetivo.
+    :type word_semantic_vector: numpy.ndarray
+    :param word_neighbors_matrix: Un diccionario que contiene dos elementos:
+                                  "neighbors_vec" que es un ndarray con los vectores de los vecinos semánticos,
+                                  y "neighbors" que es una lista de los términos correspondientes a esos vecores.
+    :type word_neighbors_matrix: dict
+    :return: Una tupla de dos ndarrays. El primero contiene las similitudes coseno ordenadas en orden descendente,
+             y el segundo contiene los términos asociados ordenados en concordancia con las similitudes coseno.
+    :rtype: tuple
+
+    Esta función primero calcula la similitud coseno entre el vector semántico de una palabra y cada uno de
+    los vectores en una matriz de vecinos. Luego, crea un array estructurado que combina estas similitudes
+    con los términos correspondientes, ordena este array por similitud coseno en orden descendente, y finalmente
+    separa y devuelve los vectores de similitudes coseno y términos en este orden.
     """
     cosine_vec = np.zeros(word_neighbors_matrix["neighbors_vec"].shape[0])
     terms = word_neighbors_matrix["neighbors"]
@@ -128,3 +199,117 @@ def neighbors_similarity(word_semantic_vector, word_neighbors_matrix):
     sorted_terms = combined_sorted['term']
     return sorted_cosines, sorted_terms
 
+def get_superterm(terms_file, gallito_code, space_name):
+    """
+    Lee una lista de términos de un archivo .txt, obtiene el vector de cada término
+    usando la función 'word_vector', suma estos vectores y devuelve el resultado y un vector
+    de similitudes coseno del vector suma con cada término.
+
+    Advertencia: Esta función es de una elevada exigencia computacional. Si se dispone del
+    vector del supertérmino y de los cosenos del supertérmino ya ordenados, se recomienda
+    no hacer uso de la misma.
+
+    :param terms_file: Ruta al archivo .txt que contiene los términos.
+    :type terms_file: str
+    :param gallito_code: Código de acceso para la API de Gallito.
+    :type gallito_code: str
+    :param space_name: Nombre del espacio semántico en la API de Gallito.
+    :type space_name: str
+    :return: Una tupla que contiene dos elementos: el primer elemento es el vector suma de todos los
+             vectores de términos, y el segundo elemento es un array numpy de similitudes coseno entre
+             el vector suma y cada uno de los vectores individuales de términos.
+    :rtype: tuple
+
+    La función lee los términos desde el archivo especificado, utiliza 'word_vector' para obtener el vector
+    semántico de cada término y los suma para formar el vector del supertérmino. Luego, calcula la similitud
+    coseno entre este vector suma y cada uno de los vectores de términos individuales y devuelve ambos, el
+    vector suma y las similitudes coseno, en una tupla.
+    """
+
+    # Inicializa el vector sumatorio y la lista de vectores individuales
+    superterm_vector = None
+    individual_vectors = []
+
+    # Leer el archivo de términos
+    with open(terms_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            term = line.strip()  # Elimina espacios y saltos de línea
+            term_vector = word_vector(term, gallito_code, space_name)
+            individual_vectors.append(term_vector)
+
+            # Sumar vectores
+            if superterm_vector is None:
+                superterm_vector = term_vector
+            else:
+                superterm_vector += term_vector
+
+    # Calcular las similitudes coseno
+    cosine_similarities = np.array([word_cosine_similarity(superterm_vector, v) for v in individual_vectors])
+
+    return superterm_vector, cosine_similarities
+
+def deserved_neighbors(word, df_h_values, sorted_cos, word_cosines):
+    """
+    Analiza y compara la similitud coseno de una palabra objetivo con un 'superterm' y
+    sus vecinos más cercanos.
+
+    :param word: Palabra objetivo para la comparación.
+    :type word: str
+    :param df_h_values: DataFrame previamente cargado que contiene los valores de diversidad
+                        semántica ('h' y 'h_dec') para las palabras.
+    :type df_h_values: pandas.DataFrame
+    :param sorted_cos: Array de NumPy con los valores de similitud coseno del 'superterm',
+                       resultante de la función get_superterm (segundo elemento de la tupla devuelta).
+    :type sorted_cos: numpy.ndarray
+    :param word_cosines: Array de NumPy con los valores de similitud coseno de la palabra objetivo
+                         con sus vecinos, resultante de la función neighbors_similarity (elemento
+                         'sorted_cosines' de la tupla devuelta).
+    :type word_cosines: numpy.ndarray
+    :return: El número de vecinos cuya similitud con la palabra objetivo es mayor o igual que la
+             similitud con el 'superterm'.
+    :rtype: int
+
+    La función calcula el peso del 'superterm' ajustado por la diversidad semántica de la palabra
+    objetivo, compara las similitudes coseno y genera un gráfico para visualizar la comparación.
+    """
+
+    # Calcular h_dec para la palabra objetivo
+    if word in df_h_values['word'].values:
+        h_dec = df_h_values[df_h_values['word'] == word]['h_dec'].iloc[0]
+    else:
+        # Encontrar la palabra más similar en df_h_values
+        closest_word, similarity_score = process.extractOne(word, df_h_values['word'].tolist())
+        h_dec = df_h_values[df_h_values['word'] == closest_word]['h_dec'].iloc[0]
+
+        # Aviso de que se ha usado una palabra similar
+        print(f"Advertencia: La palabra '{word}' no se encontró en el DataFrame. Usando '{closest_word}' (puntuación de similitud: {similarity_score}) como la más similar.")
+
+    # Calcular el superterm ponderado
+    superterm_weighted = sorted_cos / (1 + h_dec / 10)
+
+    # Aplanar los arrays para asegurar que son unidimensionales
+    sorted_cos = sorted_cos.flatten()
+    superterm_weighted = superterm_weighted.flatten()
+    word_cosines = word_cosines.flatten()
+
+    # Crear el DataFrame para el análisis
+    data = pd.DataFrame({
+        "superterm": sorted_cos[:len(word_cosines)],
+        "superterm_weighted": superterm_weighted[:len(word_cosines)],
+        "word_cosines": word_cosines,
+        "secuencia": np.arange(1, len(word_cosines) + 1)
+    })
+
+    sum_value = sum(data['word_cosines'] >= data['superterm_weighted'])
+
+    # Generar el gráfico
+    plt.figure(figsize=(10, 6))
+    plt.plot(data['secuencia'], data['superterm_weighted'], label='Weighted Superterm')
+    plt.plot(data['secuencia'], data['word_cosines'], label=word)
+    plt.xlabel('Number of Neighbors')
+    plt.ylabel('Similarity')
+    plt.title(f'Superterm vs {word.capitalize()} Cosine Similarities')
+    plt.legend()
+    plt.show()
+
+    return sum_value
--- tests/test_contours.py
+++ tests/test_contours.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
-from QSub.contours import gallito_neighbors_matrix, word_vector, word_cosine_similarity, neighbors_similarity
+import pandas as pd
+from QSub.contours import gallito_neighbors_matrix, word_vector, word_cosine_similarity, neighbors_similarity, get_superterm, deserved_neighbors
 
 
 def test_gallito_neighbors_matrix():
@@ -69,3 +70,47 @@ def test_neighbors_similarity():
     assert type(resultado) == tuple  # Comprueba que el resultado es una tupla
     assert type(resultado[0]) == np.ndarray # Comprueba que los elementos de la tupla son ndarrays
     assert type(resultado[1]) == np.ndarray
+
+def test_get_superterm():
+    # Parámetros de prueba
+    test_vocabulary_path = "/home/alex/Documents/RESEARCH/QSub/resources/vocabulario_test_sp.txt" # Cambiar la ruta absoluta para tests en otros dispositivos
+    test_code = os.getenv('GALLITO_API_KEY', 'default_code')
+    test_space_name = "quantumlikespace_spanish"
+
+    # Ejecutamos la función
+    resultado = get_superterm(test_vocabulary_path, test_code, test_space_name)
+
+    # Comprobamos los tests
+    assert len(resultado) == 2  # Comprueba que el resultado tenga dos elementos
+    assert type(resultado) == tuple  # Comprueba que el resultado es una tupla
+    assert type(resultado[0]) == np.ndarray # Comprueba que los elementos de la tupla son ndarrays
+    assert type(resultado[1]) == np.ndarray
+
+def test_deserved_neighbors():
+    # Parámetros de prueba
+    test_h_df_path = "/home/alex/Documents/RESEARCH/QSub/resources/sp_vocab_semantic_diversity.csv" # Cambiar la ruta absoluta para tests en otros dispositivos
+    test_superterm_cosines_path = "/home/alex/Documents/RESEARCH/QSub/resources/superterm_cosines.csv"  # Cambiar la ruta absoluta para tests en otros dispositivos
+    test_word = "china"
+    test_code = os.getenv('GALLITO_API_KEY', 'default_code')
+    test_space_name = "quantumlikespace_spanish"
+    test_neighbors = 100
+    test_space_dimensions = 300
+
+    # Importaciones de prueba
+    test_h_df = pd.read_csv(test_h_df_path)  # Asegúrate de usar la ruta correcta al archivo
+    test_h_df = test_h_df.drop(test_h_df.columns[0], axis=1)
+    test_superterm_cosines = pd.read_csv(test_superterm_cosines_path)
+    test_superterm_cosines = test_superterm_cosines.drop(test_superterm_cosines.columns[0], axis=1)
+    test_superterm_cosines = np.array(test_superterm_cosines)
+
+    # Ejecutamos la función
+    neighbors = gallito_neighbors_matrix(test_word, test_code, test_space_name, neighbors=test_neighbors,
+                                         space_dimensions=test_space_dimensions)
+    word = word_vector(test_word, test_code, test_space_name)
+    word_cosines = neighbors_similarity(word, neighbors)
+    word_cosines = word_cosines[0]
+    resultado = deserved_neighbors("chino", test_h_df, test_superterm_cosines, word_cosines)
+
+    # Comprobamos los tests
+    assert resultado > 0  # Comprueba que el resultado es mayor que 0
+    assert type(resultado) == int  # Comprueba que el resultado es un int
