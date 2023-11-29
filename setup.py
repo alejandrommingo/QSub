fcd19e8340810830e275743458ffee6aedbd2d31
amartinezm90 <a.martinez.m90@gmail.com>
2023-11-29 14:19:57 +0100

    Resources available

--- setup.py
+++ setup.py
@@ -8,9 +8,18 @@ setup(
         'numpy',
         'pandas',
         'requests',
+        're',
+        'matplotlib',
+        'fuzzywuzzy',
+        'html',
+        'xml.etree.ElementTree'
         'transformers',
         'gensim',
         'pytest'
     ],
+    include_package_data=True,  # Asegúrate de incluir esto
+    package_data={
+        'QSub': ['resources/*']  # Incluye todos los archivos en la carpeta resources
+    }
     # Más configuraciones como author, license, etc.
 )
